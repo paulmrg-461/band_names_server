a9131f346969c37e3acc265752877737b55a49f1
Paul Realpe <paulmrg461@gmail.com>
2021-01-20 12:23:17 -0500

    20. Initial config - Socket.io

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 require('dotenv').config();
 
+//Express App
+const app = express();
+
+// Node Server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Socket Messages
+io.on('connection', client => {
+    console.log('User online')
+    client.on('disconnect', () => { 
+        console.log('User offline')
+     });
+  });
+
 //Public path
 const publicPath = path.resolve( __dirname, 'public' );
-
 app.use( express.static( publicPath ));
 
-app.listen( process.env.PORT, ( err ) => {
+server.listen( process.env.PORT, ( err ) => {
     if ( err ) throw new Error( err ); 
     console.log('Running server in port: ', process.env.PORT);
 });
